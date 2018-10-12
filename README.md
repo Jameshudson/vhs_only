18d80ee506b7c5c61d2916266311d208a7637397
James Hudson <james@nimbushosting.co.uk>
2018-10-12 13:45:06 +0100

    Adding index

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vhs_only
 A small Go project that lists films that have not been released on DVD or blu ray
+
+Run
+go run !(*_test).go 
--- routes/api.go
+++ routes/api.go
@@ -2,14 +2,16 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	// "github.com/joho/godotenv/cmd/godotenv"
+	"github.com/jameshudson/vhs_only/http/controller"
+	// "fmt"
 )
 
 func ApiRoutes(routes *gin.Engine) {
 
-	routes.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// filmController := new(controller.FilmContoller)
+
+	routes.GET("/films", controller.Index)
+	routes.GET("/films/:title", controller.Get)
 
 }
